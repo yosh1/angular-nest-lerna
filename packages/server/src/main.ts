5f35fc2d968ce82c3b8e34d2e15ed9d42152996f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-12-03 01:54:23 +0900

    ✨coding): add api that hello world!

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// add start
+import * as express from 'express';
+import { join } from 'path';
+// add end
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // add start
+  app.setGlobalPrefix('api');
+
+  app.use(express.static(join(__dirname, '..', 'client')));
+  app.use('/*', express.static(join(__dirname, '..', 'client/index.html')));
+  // add end
+
   await app.listen(3000);
 }
 bootstrap();
